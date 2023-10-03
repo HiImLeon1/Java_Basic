1e306afb1ff8d36fd79a8d5d75349006c2d940e8
Sangbong Lee <tkdqhd6672@gmail.com>
2023-10-03 18:26:00 +0900

    first commit

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_Country_CODE = "+82"; //
+//        KR_Country_CODE = "+8282";
+        System.out.println(KR_Country_CODE);
+
+        final double PI = 3.14159265358979;
+        final String DATE_OF_BIRTH = "1999-05-19";
+
+
+
+    }
+}
---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,16 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+
+        String bus = "상암08";
+        int min = 3;
+        float km = 1.2f;
+        System.out.println(bus + "번 버스");
+        System.out.println("약 " + min + "분 후 도착");
+        System.out.println("남은 거리 " + km + "km" );
+
+
+
+    }
+}
