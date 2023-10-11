36ab9e9ef9b678cc6e6e0f7147b5bab9706647cc
SangBong Lee <146823015+HiImLeon1@users.noreply.github.com>
2023-10-11 13:39:31 +0000

    Add files via upload

---
+++ src/studyAlone/Main.java
@@ -0,0 +1,38 @@
+package studyAlone;
+
+public class Main {
+    int radius;
+    String name;
+
+    public Main() {
+        radius = 1; name = "";
+
+    }
+    public Main(int r, String n) {
+        radius = r; name = n;
+
+    }
+    public double getArea() {
+        return 3.14*radius*radius;
+    }
+
+    public static void main(String [] args) {
+        Main pizza;
+        pizza = new Main();
+        pizza.radius = 10;
+        pizza.name = "게살피자";
+        double area = pizza.getArea();
+        System.out.println(pizza.name + "의 면적" + area);
+
+        Main donut;
+        donut = new Main();
+        donut.radius = 2;
+        donut.name = "던킨";
+        area = donut.getArea();
+        System.out.println(donut.name + "의 면적은" + area);
+        System.out.println("종료");
+
+    }
+
+
+}
