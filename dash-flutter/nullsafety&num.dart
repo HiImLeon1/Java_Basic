2a6c166efe76d4214a64ae89e5f01b24df957721
SangBong Lee <146823015+HiImLeon1@users.noreply.github.com>
2023-10-31 15:52:02 +0900

    Create nullsafety&num.dart

---
+++ dash-flutter/nullsafety&num.dart
@@ -0,0 +1,23 @@
+String hi({
+  
+           required String name,
+           required int age,
+           required String country,
+  
+          }){
+  
+  return 'Hello my name is $name and i`m $age old and came from $country';
+//화살표 함수 (fat arrow syntax)
+}
+
+num plus (num a, num b) => a + b;
+
+void main() {
+  print(plus(3,4));
+  print(hi(
+  age : 12,
+  country : 'kor',
+  name : 'Lee',
+  ));
+  
+}
